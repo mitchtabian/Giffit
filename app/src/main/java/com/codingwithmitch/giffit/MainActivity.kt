5ab77aed4b8f9916a714f431076b68aabfa66515
mitchtabian <mitch@tabian.ca>
2022-08-16 15:55:51 -0700

    advanced gestures example

--- app/src/main/java/com/codingwithmitch/giffit/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/giffit/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,14 +13,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.dp
 import com.codingwithmitch.giffit.ui.theme.GiffitTheme
+import java.lang.Math.*
 
 class MainActivity : ComponentActivity() {
 
@@ -36,27 +35,59 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize()
                     ) {
                         var offset by remember { mutableStateOf(Offset.Zero) }
-                        val offsetXDp = with(LocalDensity.current) { offset.x.toDp() }
-                        val offsetYDp = with(LocalDensity.current) { offset.y.toDp() }
+                        var zoom by remember { mutableStateOf(1f) }
+                        var angle by remember { mutableStateOf(0f) }
                         Box(
                             modifier = Modifier
-                                .width(50.dp)
-                                .height(50.dp)
-                                .offset(
-                                    x = offsetXDp,
-                                    y = offsetYDp
-                                )
-                                .clip(RectangleShape)
-                                .background(Color.Black)
                                 .pointerInput(Unit) {
-                                    detectDragGestures { change, dragAmount ->
-                                        offset += dragAmount
-                                    }
+                                    detectTransformGestures(
+                                        onGesture = { centroid, pan, gestureZoom, gestureRotate ->
+                                            val oldScale = zoom
+                                            val newScale = zoom * gestureZoom
+
+                                            // For natural zooming and rotating, the centroid of the gesture should
+                                            // be the fixed point where zooming and rotating occurs.
+                                            // We compute where the centroid was (in the pre-transformed coordinate
+                                            // space), and then compute where it will be after this delta.
+                                            // We then compute what the new offset should be to keep the centroid
+                                            // visually stationary for rotating and zooming, and also apply the pan.
+                                            offset = (offset + centroid / oldScale).rotateBy(gestureRotate) -
+                                                    (centroid / newScale + pan / oldScale)
+                                            zoom = newScale
+                                            angle += gestureRotate
+                                        }
+                                    )
                                 }
+                                .graphicsLayer {
+                                    translationX = -offset.x * zoom
+                                    translationY = -offset.y * zoom
+                                    scaleX = zoom
+                                    scaleY = zoom
+                                    rotationZ = angle
+                                    transformOrigin = TransformOrigin(0f, 0f)
+                                }
+                                .background(Color.Blue)
+                                .fillMaxSize()
                         )
                     }
                 }
             }
         }
     }
+}
+
+/**
+ * Rotates the given offset around the origin by the given angle in degrees.
+ *
+ * A positive angle indicates a counterclockwise rotation around the right-handed 2D Cartesian
+ * coordinate system.
+ *
+ * See: [Rotation matrix](https://en.wikipedia.org/wiki/Rotation_matrix)
+ */
+fun Offset.rotateBy(angle: Float): Offset {
+    val angleInRadians = angle * PI / 180
+    return Offset(
+        (x * cos(angleInRadians) - y * sin(angleInRadians)).toFloat(),
+        (x * sin(angleInRadians) + y * cos(angleInRadians)).toFloat()
+    )
 }
