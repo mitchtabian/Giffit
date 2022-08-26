6335ee219843c5d989d2982391d1ea051940a7f8
mitchtabian <mitch@tabian.ca>
2022-08-25 18:56:03 -0700

    Fix gesture issues https://issuetracker.google.com/issues/233251825

--- app/src/main/java/com/codingwithmitch/giffit/ui/compose/BackgroundAsset.kt
+++ app/src/main/java/com/codingwithmitch/giffit/ui/compose/BackgroundAsset.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
@@ -21,10 +20,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.fastAny
-import androidx.compose.ui.util.fastForEach
 import androidx.compose.ui.zIndex
 import androidx.constraintlayout.compose.ConstraintLayout
 import coil.compose.AsyncImage
@@ -33,7 +29,6 @@ import com.codingwithmitch.giffit.domain.util.AssetData
 import com.codingwithmitch.giffit.domain.DataState
 import com.codingwithmitch.giffit.ui.compose.RecordActionBar
 import kotlin.math.PI
-import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -195,10 +190,6 @@ fun RenderAsset(
     var offset by remember { mutableStateOf(assetData.initialOffset) }
     var zoom by remember { mutableStateOf(1f) }
     var angle by remember { mutableStateOf(0f) }
-    var size by remember { mutableStateOf(assetData.initialSize) }
-
-    val xOffset = with(LocalDensity.current) { -offset.x.toDp() }
-    val yOffset = with(LocalDensity.current) { -offset.y.toDp() }
 
     Box (
         modifier = Modifier
@@ -208,164 +199,37 @@ fun RenderAsset(
         val painter: Painter = rememberAsyncImagePainter(model = assetData.id)
         Image(
             modifier = Modifier
-                .size(width = size.width.dp, height = size.height.dp)
-                .offset(
-                    x = xOffset,
-                    y = yOffset
-                )
+                .graphicsLayer {
+                    val rotatedOffset = offset.rotateBy(angle)
+                    translationX = -rotatedOffset.x
+                    translationY = -rotatedOffset.y
+                    scaleX = zoom
+                    scaleY = zoom
+                    rotationZ = angle
+                    transformOrigin = TransformOrigin(0f, 0f)
+                }
                 .pointerInput(Unit) {
-                    detectTransformGesturesAndTouch(
+                    // Shout-out to the dude who posted on issue tracker https://issuetracker.google.com/issues/233251825
+                    detectTransformGestures(
                         onGesture = { centroid, pan, gestureZoom, gestureRotate ->
                             val oldScale = zoom
                             val newScale = zoom * gestureZoom
-                            var newSize = size * gestureZoom
-
-                            // Restrict the height
-                            if (newSize.height >= assetContainerHeightDp) {
-                                newSize = Size(
-                                    width = newSize.width,
-                                    height = assetContainerHeightDp.toFloat()
-                                )
-                            }
-
-                            // Restrict the width
-                            if (newSize.width >= screenWidthDp) {
-                                newSize = Size(
-                                    width = screenWidthDp.toFloat(),
-                                    height = newSize.height
-                                )
-                            }
-
-                            // If we're only moving the shape we need to put bounds on how fast it can move.
-                            // Otherwise as it gets smaller it will move faster. And as it gets bigger it will
-                            // move slower.
-                            // If zoom has not changed, limit movement speed.
-                            val newOffset = if (newSize == size) {
-                                offset - pan
-                            } else { // We're zooming/rotating
-                                // https://developer.android.com/reference/kotlin/androidx/compose/foundation/gestures/package-summary#(androidx.compose.ui.input.pointer.PointerInputScope).detectTransformGestures(kotlin.Boolean,kotlin.Function4)
-                                (offset + centroid / oldScale).rotateBy(gestureRotate) -
-                                        (centroid / newScale + pan / oldScale)
-                            }
-                            offset = newOffset
-
-                            // Restrict the offset
-                            val a = size.width.dp / 2
-                            val b = size.height.dp / 2
-
-                            // Start bound
-                            if (offset.x.toDp() >= a) {
-                                offset = Offset(
-                                    x = a.toPx(),
-                                    y = offset.y
-                                )
-                            }
-
-                            // Top bound
-                            if (offset.y.toDp() >= b) {
-                                offset = Offset(
-                                    x = offset.x,
-                                    y = b.toPx()
-                                )
-                            }
-
-                            // End-x bound
-                            val endXBound = (-screenWidthDp.dp + a)
-                            if (offset.x.toDp() < endXBound) {
-                                offset = Offset(
-                                    x = endXBound.toPx(),
-                                    y = offset.y
-                                )
-                            }
-
-                            // Bottom-y bound
-                            val bottomYBound = (-assetContainerHeightDp.dp + b)
-                            if (offset.y.toDp() < bottomYBound) {
-                                offset = Offset(
-                                    x = offset.x,
-                                    y = bottomYBound.toPx()
-                                )
-                            }
-
-                            size = newSize
-                            zoom = newScale
                             angle += gestureRotate
+                            zoom = newScale
+                            offset = (offset - centroid * oldScale).rotateBy(-gestureRotate) +
+                                    (centroid * newScale - pan * oldScale)
                         }
                     )
                 }
-                .graphicsLayer {
-                    rotationZ = angle
-                    transformOrigin = TransformOrigin.Center
-                }
+                .size(
+                    width = assetData.initialSize.width.dp,
+                    height = assetData.initialSize.height.dp
+                )
                 .zIndex(1f)
             ,
             painter = painter,
             contentDescription = ""
         )
-    }
-}
-
-private suspend fun PointerInputScope.detectTransformGesturesAndTouch(
-    panZoomLock: Boolean = false,
-    onGesture: (centroid: Offset, pan: Offset, zoom: Float, rotation: Float) -> Unit
-) {
-    forEachGesture {
-        awaitPointerEventScope {
-            var rotation = 0f
-            var zoom = 1f
-            var pan = Offset.Zero
-            var pastTouchSlop = false
-            val touchSlop = viewConfiguration.touchSlop
-            var lockedToPanZoom = false
-
-            awaitFirstDown(requireUnconsumed = false)
-
-            do {
-                val event = awaitPointerEvent()
-
-                val canceled = event.changes.fastAny { it.positionChangeConsumed() }
-                if (!canceled) {
-                    val zoomChange = event.calculateZoom()
-                    val rotationChange = event.calculateRotation()
-                    val panChange = event.calculatePan()
-
-                    if (!pastTouchSlop) {
-                        zoom *= zoomChange
-                        rotation += rotationChange
-                        pan += panChange
-
-                        val centroidSize = event.calculateCentroidSize(useCurrent = false)
-                        val zoomMotion = abs(1 - zoom) * centroidSize
-                        val rotationMotion = abs(rotation * PI.toFloat() * centroidSize / 180f)
-                        val panMotion = pan.getDistance()
-
-                        if (zoomMotion > touchSlop ||
-                            rotationMotion > touchSlop ||
-                            panMotion > touchSlop
-                        ) {
-                            pastTouchSlop = true
-                            lockedToPanZoom = panZoomLock && rotationMotion < touchSlop
-                        }
-                    }
-
-                    if (pastTouchSlop) {
-                        val centroid = event.calculateCentroid(useCurrent = false)
-                        val effectiveRotation = if (lockedToPanZoom) 0f else rotationChange
-                        if (effectiveRotation != 0f ||
-                            zoomChange != 1f ||
-                            panChange != Offset.Zero
-                        ) {
-                            onGesture(centroid, panChange, zoomChange, effectiveRotation)
-                        }
-                        event.changes.fastForEach {
-                            if (it.positionChanged()) {
-                                it.consumeAllChanges()
-                            }
-                        }
-                    }
-                }
-            } while (!canceled && event.changes.fastAny { it.pressed })
-        }
     }
 }
 
