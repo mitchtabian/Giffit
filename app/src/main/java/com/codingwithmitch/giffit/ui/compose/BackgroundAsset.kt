65328704fadb9d1f8cc0e8b68bc5693c9c32e396
mitchtabian <mitch@tabian.ca>
2022-08-26 15:49:18 -0700

    Render drawables over other drawables.

--- app/src/main/java/com/codingwithmitch/giffit/ui/compose/BackgroundAsset.kt
+++ app/src/main/java/com/codingwithmitch/giffit/ui/compose/BackgroundAsset.kt
@@ -1,7 +1,6 @@
 package com.codingwithmitch.giffit.ui.compose
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
@@ -11,7 +10,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
@@ -39,44 +37,56 @@ fun RenderBackground(
             contentScale = ContentScale.Crop,
             contentDescription = ""
         )
-        RenderAsset()
+        RenderAsset(
+            assetContainerHeightDp = assetContainerHeightDp
+        )
     }
 }
 
 @Composable
-fun RenderAsset() {
+fun RenderAsset(
+    assetContainerHeightDp: Int
+) {
     var offset by remember { mutableStateOf(Offset.Zero) }
     var zoom by remember { mutableStateOf(1f) }
     var angle by remember { mutableStateOf(0f) }
-    Box(
-        modifier = Modifier
-            .graphicsLayer {
-                // Change 1
-                val rotatedOffset = offset.rotateBy(angle)
-                translationX = -rotatedOffset.x
-                translationY = -rotatedOffset.y
-                scaleX = zoom
-                scaleY = zoom
-                rotationZ = angle
-                transformOrigin = TransformOrigin(0f, 0f)
-            }
-            .pointerInput(Unit) {
-                detectTransformGestures(
-                    onGesture = { centroid, pan, gestureZoom, gestureRotate ->
-                        val oldScale = zoom
-                        val newScale = zoom * gestureZoom
-                        angle += gestureRotate
-                        zoom = newScale
 
-                        // Change 2
-                        offset = (offset - centroid * oldScale).rotateBy(-gestureRotate) +
-                                (centroid * newScale - pan * oldScale)
-                    }
-                )
-            }
-            .background(Color.Blue)
-            .size(200.dp, 200.dp)
-    )
+    val asset = painterResource(R.drawable.deal_with_it_sunglasses_default)
+
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(assetContainerHeightDp.dp)
+    ) {
+        Image(
+            modifier = Modifier
+                .graphicsLayer {
+                    val rotatedOffset = offset.rotateBy(angle)
+                    translationX = -rotatedOffset.x
+                    translationY = -rotatedOffset.y
+                    scaleX = zoom
+                    scaleY = zoom
+                    rotationZ = angle
+                    transformOrigin = TransformOrigin(0f, 0f)
+                }
+                .pointerInput(Unit) {
+                    detectTransformGestures(
+                        onGesture = { centroid, pan, gestureZoom, gestureRotate ->
+                            val oldScale = zoom
+                            val newScale = zoom * gestureZoom
+                            angle += gestureRotate
+                            zoom = newScale
+                            offset = (offset - centroid * oldScale).rotateBy(-gestureRotate) +
+                                    (centroid * newScale - pan * oldScale)
+                        }
+                    )
+                }
+                .size(200.dp, 200.dp)
+            ,
+            painter = asset,
+            contentDescription = ""
+        )
+    }
 }
 
 /**
