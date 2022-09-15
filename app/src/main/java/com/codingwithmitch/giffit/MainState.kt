7c519affa2fb820d68f14788d32fe9d181721b23
mitch <mitch@tabian.ca>
2022-09-14 18:27:03 -0700

    patch

--- app/src/main/java/com/codingwithmitch/giffit/MainState.kt
+++ app/src/main/java/com/codingwithmitch/giffit/MainState.kt
@@ -2,16 +2,13 @@ package com.codingwithmitch.giffit
 
 import android.graphics.Bitmap
 import android.net.Uri
-import androidx.activity.result.ActivityResultLauncher
 import androidx.compose.ui.geometry.Rect
 
 sealed class MainState {
 
     object Initial: MainState()
 
-    data class DisplaySelectBackgroundAsset(
-        val backgroundAssetPickerLauncher: ActivityResultLauncher<String>
-    ): MainState()
+    object DisplaySelectBackgroundAsset: MainState()
 
     data class DisplayBackgroundAsset(
         val backgroundAssetUri: Uri,
