44760629e0c4b2eb48ddb170c3e292437661ea50
mitch <mitch@tabian.ca>
2022-09-14 18:22:12 -0700

    remove

--- app/src/main/java/com/codingwithmitch/giffit/MainActivityUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.codingwithmitch.giffit
-
-import android.net.Uri
-import androidx.activity.result.contract.ActivityResultContracts
-import com.canhub.cropper.CropImageContract
-import com.canhub.cropper.CropImageView
-import com.canhub.cropper.options
